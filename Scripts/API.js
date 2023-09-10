c842244796632d105e107470d3a78e3a51f0b89e
German Shein <germanshein1995@gmail.com>
2023-09-11 00:53:41 +0400

    API: Referencing only the first element of class-based elements

--- Scripts/API.js
+++ Scripts/API.js
@@ -11,11 +11,11 @@ const Send_Email = async (Language) =>
 {
 	const Response = await fetch (`${API_Endpoint}/email`, { method: 'POST',  headers: { 'Content-Type': 'application/json' }, body: JSON.stringify ({ Name: document.getElementsByClassName ('Input_Field') [0].value, Email: document.getElementsByClassName ('Input_Field') [1].value, Subject: Language === 'ar' ? `${document.querySelector ('.Selection_Controller:checked').value} طلب` : `Request for ${document.querySelector ('.Selection_Controller:checked').value}`, Message: document.getElementsByClassName ('Input_Field') [2].value, Language })});
 	const Response_Body = await Response.json ();
-	document.getElementsByClassName ('Contact_Us_Response').classList.remove ('Display_None');
+	document.getElementsByClassName ('Contact_Us_Response') [0].classList.remove ('Display_None');
 	if (Response_Body.Status === 201)
 	{
 		document.getElementById ('Contact_Us_Header').classList.add ('Display_None');
-		document.getElementsByClassName ('Contact_Us_Form').classList.add ('Display_None');
+		document.getElementsByClassName ('Contact_Us_Form') [0].classList.add ('Display_None');
 		document.getElementById ('Call_to_Action_2').classList.add ('Display_None');
 		if ([...document.getElementById ('Email_Icon').classList].includes ('Display_None'))
 		{
